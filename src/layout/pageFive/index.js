c4793d8d86261147d827a709cc66efa26fdeddef
jhernandez-11 <jhernandez11@protonmail.com>
2022-06-15 23:38:33 -0700

    init?

--- src/layout/pageFive/index.js
+++ src/layout/pageFive/index.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 const pageFive = (props) => (
     <div id='pageFive' className={styles.pageFive}>
         <div className={styles.pageFive__textContainer}>
-            <p className={styles.pageFive__textContainer__heading}>I go by my alias <span className={styles.pageFive__textContainer__heading__span}>Hyouka</span>: I&apos;m a web developer and graphic artist. As a recent college graduate,  I started this project solo but will expand with a team in hopes of seeing my vision fruition. In addition to being a savvy tech sorcerer, I also enjoy watching anime, reading novels, and traveling.</p>
+            <p className={styles.pageFive__textContainer__heading}>Known as my alias <span className={styles.pageFive__textContainer__heading__span}>Hyouka</span>: I&apos;m a web developer and graphic artist. As a recent college graduate,  I started this project solo but will expand with a team in hopes of seeing my vision fruition. In addition to being a savvy tech sorcerer, I also enjoy watching anime, reading novels, and traveling.</p>
         </div>
         <div className={styles.pageFive__profileContainer}>
-            <Image src='/sql-prof-pic.png' layout="fill" alt='hyouka pic' />
+            <Image src='/fushi.png' layout="fill" alt='hyouka pic' />
         </div>
     </div>
 )
--- src/layout/pageSix/index.js
+++ src/layout/pageSix/index.js
@@ -3,9 +3,7 @@ import Image from 'next/image'
 
 const pageSix = (props) => (
     <div className={styles.pageSix}>
-        <div className={styles.pageSix__imgCont}>
-            <Image src='/meta-banners_logo.png' height={150} width={150} alt="logo" />
-        </div>
+        
     </div>
 )
 
