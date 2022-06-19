2ace51b2b85717d3ecf49e6bfb5b6dcceb461c43
jhernandez-11 <jhernandez11@protonmail.com>
2022-06-19 16:04:18 -0700

    final

--- src/layout/pageSix/index.js
+++ src/layout/pageSix/index.js
@@ -3,7 +3,12 @@ import Image from 'next/image'
 
 const pageSix = (props) => (
     <div className={styles.pageSix}>
-        
+        <p>Meta Banners</p>
+        <div className={styles.footer}>
+            <h1>Follow &#9733;</h1>
+            <a href="#"><ion-icon name="logo-twitter" style={{fontSize: '3.2rem', marginRight: '1.5rem', color: 'white'}}></ion-icon></a>
+            <a href="#"><ion-icon name="logo-instagram" style={{fontSize: '3.2rem', color: 'white'}}></ion-icon></a>
+        </div>
     </div>
 )
 
